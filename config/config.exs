b6055cb2dd6fafe6f086ed04af18ea79a06148bf
DROO <andrewtehsailor@gmail.com>
2025-04-04 01:46:29 +0200

    chore: update all dependencies to latest versions

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
@@ -30,8 +30,6 @@ config :logger, level: :debug
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
-
-import Config
 
 # Configure your database
 config :raxol, Raxol.Repo,
