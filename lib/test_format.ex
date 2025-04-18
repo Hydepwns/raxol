e0a679e3da6314a9029644a4e38b3379b85c8626
DROO <andrewtehsailor@gmail.com>
2025-04-18 15:39:54 +0200

    Test improved pre-commit format hook

--- lib/test_format.ex
+++ lib/test_format.ex
@@ -1,13 +1,13 @@
-defmodule Raxol.TestFormat do
+defmodule   Raxol.TestFormat do
   @moduledoc """
   This is a test module for demonstrating the pre-commit formatting hook.
   """
 
-    def test_function(  a,    b  ) do
+    def     test_function(  a,    b  ) do
         a   +  b
     end
 
   def another_test(x) when is_integer(x)  do
-    x*2
+    x    *      2
   end
 end
