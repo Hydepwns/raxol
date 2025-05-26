8061f6829fcb5d549698b6b51b170ba557080711
DROO <andrewtehsailor@gmail.com>
2025-05-26 14:57:45 +0200

    test(terminal): add telemetry emission tests for scroll and paste events

---
+++ test/raxol/terminal/manager_test.exs
@@ -0,0 +1,183 @@
+defmodule Raxol.Terminal.ManagerTest do
+  use ExUnit.Case, async: true
+
+  alias Raxol.Terminal.Manager
+  alias Raxol.Core.Events.Event
+
+  defmodule DummyEmulator do
+    defstruct []
+    def process_input(emulator, _event), do: {emulator, :ok}
+    def get_active_buffer(_emulator), do: %{}
+    def update_active_buffer(emulator, _buffer), do: emulator
+  end
+
+  setup do
+    {:ok, pid} =
+      Manager.start_link(terminal: %DummyEmulator{}, runtime_pid: self())
+
+    %{pid: pid}
+  end
+
+  test "window resize event triggers notify_resized", %{pid: pid} do
+    event = %Event{
+      type: :window,
+      data: %{action: :resize, width: 100, height: 40}
+    }
+
+    Manager.process_event(pid, event)
+    assert_received {:terminal_resized, 100, 40}
+  end
+
+  test "window focus event triggers notify_focus_changed", %{pid: pid} do
+    event = %Event{type: :window, data: %{action: :focus, focused: true}}
+    Manager.process_event(pid, event)
+    assert_received {:terminal_focus_changed, true}
+  end
+
+  test "window blur event triggers notify_focus_changed(false)", %{pid: pid} do
+    event = %Event{type: :window, data: %{action: :blur}}
+    Manager.process_event(pid, event)
+    assert_received {:terminal_focus_changed, false}
+  end
+
+  test "mode event triggers notify_mode_changed", %{pid: pid} do
+    event = %Event{type: :mode, data: %{mode: :insert}}
+    Manager.process_event(pid, event)
+    assert_received {:terminal_mode_changed, :insert}
+  end
+
+  test "focus event triggers notify_focus_changed", %{pid: pid} do
+    event = %Event{type: :focus, data: %{focused: false}}
+    Manager.process_event(pid, event)
+    assert_received {:terminal_focus_changed, false}
+  end
+
+  test "clipboard event triggers notify_clipboard_event", %{pid: pid} do
+    event = %Event{type: :clipboard, data: %{op: :copy, content: "abc"}}
+    Manager.process_event(pid, event)
+    assert_received {:terminal_clipboard_event, :copy, "abc"}
+  end
+
+  test "selection event triggers notify_selection_changed", %{pid: pid} do
+    event = %Event{
+      type: :selection,
+      data: %{start_pos: {0, 0}, end_pos: {1, 1}, text: "hi"}
+    }
+
+    Manager.process_event(pid, event)
+
+    assert_received {:terminal_selection_changed,
+                     %{start_pos: {0, 0}, end_pos: {1, 1}, text: "hi"}}
+  end
+
+  test "paste event triggers notify_paste_event", %{pid: pid} do
+    event = %Event{type: :paste, data: %{text: "foo", position: {2, 3}}}
+    Manager.process_event(pid, event)
+    assert_received {:terminal_paste_event, "foo", {2, 3}}
+  end
+
+  test "cursor event triggers notify_cursor_event", %{pid: pid} do
+    event = %Event{
+      type: :cursor,
+      data: %{visible: true, style: :block, blink: true, position: {1, 2}}
+    }
+
+    Manager.process_event(pid, event)
+
+    assert_received {:terminal_cursor_event,
+                     %{
+                       visible: true,
+                       style: :block,
+                       blink: true,
+                       position: {1, 2}
+                     }}
+  end
+
+  test "scroll event triggers notify_scroll_event", %{pid: pid} do
+    event = %Event{
+      type: :scroll,
+      data: %{direction: :down, delta: 5, position: {0, 0}}
+    }
+
+    Manager.process_event(pid, event)
+    assert_received {:terminal_scroll_event, :down, 5, {0, 0}}
+  end
+
+  test "unknown event type does not crash", %{pid: pid} do
+    event = %Event{type: :unknown, data: %{foo: :bar}}
+    assert :ok = Manager.process_event(pid, event)
+    refute_received _
+  end
+
+  test "missing terminal returns error", _ do
+    {:ok, pid} = Manager.start_link([])
+
+    event = %Event{
+      type: :window,
+      data: %{action: :resize, width: 10, height: 10}
+    }
+
+    assert {:error, :no_terminal} = Manager.process_event(pid, event)
+
+    assert_received {:terminal_error, :no_terminal,
+                     %{action: :process_event, event: ^event}}
+  end
+
+  describe "telemetry event emission" do
+    setup do
+      test_pid = self()
+      ref = make_ref()
+
+      events = [
+        [:raxol, :terminal, :scroll_event, :delta],
+        [:raxol, :terminal, :paste_event, :length]
+      ]
+
+      handler_id = "test-telemetry-handler-#{System.unique_integer()}"
+
+      :telemetry.attach_many(
+        handler_id,
+        events,
+        fn event, measurements, metadata, _config ->
+          send(test_pid, {:telemetry_event, event, measurements, metadata, ref})
+        end,
+        nil
+      )
+
+      on_exit(fn -> :telemetry.detach(handler_id) end)
+      %{ref: ref}
+    end
+
+    test "emits scroll_event.delta telemetry", %{pid: pid, ref: ref} do
+      event = %Event{
+        type: :scroll,
+        data: %{direction: :vertical, delta: 7, position: {1, 2}}
+      }
+
+      Manager.process_event(pid, event)
+
+      assert_receive {
+                       :telemetry_event,
+                       [:raxol, :terminal, :scroll_event, :delta],
+                       %{delta: 7},
+                       %{direction: :vertical, position: {1, 2}, pid: _},
+                       ^ref
+                     },
+                     100
+    end
+
+    test "emits paste_event.length telemetry", %{pid: pid, ref: ref} do
+      event = %Event{type: :paste, data: %{text: "foobar", position: {3, 4}}}
+      Manager.process_event(pid, event)
+
+      assert_receive {
+                       :telemetry_event,
+                       [:raxol, :terminal, :paste_event, :length],
+                       %{length: 6},
+                       %{position: {3, 4}, pid: _},
+                       ^ref
+                     },
+                     100
+    end
+  end
+end
