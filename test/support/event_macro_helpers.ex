23bc24502da179d2cdb58ca555ef8c55065fb96a
Test User <test@example.com>
2025-07-03 16:51:15 +0200

    chore: remove obsolete and redundant test/support files
    - Delete unused or obsolete test helpers and mode manager test
    - Remove old base component test for UI components

--- test/support/event_macro_helpers.ex
+++
@@ -1,7 +0,0 @@
-defmodule EventMacroHelpers do
-  defmacro delegate_handle_event_3_to_2 do
-    quote do
-      def handle_event(event, state, _context), do: handle_event(event, state)
-    end
-  end
-end
